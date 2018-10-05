d580349a1d038bd9e69ef7149946104b4b942ed6
Brian Birdwell <bbirdwell@gmail.com>
2018-10-05 19:10:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,20 @@
 class Song
   attr_accessor :name, :genre, :artist
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def artist_name
     self.artist.name
   end
+  
+  def self.all
+    @@all
+  end
+  
   
 end
